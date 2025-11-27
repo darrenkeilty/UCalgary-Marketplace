c9e7467af1920275c68b588b79c834a90fc3b2b8
Imdad Goraho <imdad.goraho@ucalgary.ca>
2025-11-26 20:19:42 -0700

    feat: register report routes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ import mySettingsRoutes from "./routes/userSettingsRoute/mySettingsRoute.js";
 import myContactedRoutes from "./routes/userSettingsRoute/myContactedRoute.js";
 import myPostsRoutes from "./routes/userSettingsRoute/myPostsRoute.js";
 import myEventsRoute from "./routes/userSettingsRoute/myEventsRoute.js";
+import reportRoutes from "./routes/reportRoutes/reportRoutes.js";
 
 const app = express();
 
@@ -47,5 +48,8 @@ app.use("/api/my-posts", myPostsRoutes);
 
 // My events post route
 app.use("/api/my-events", myEventsRoute);
+
+// Report route
+app.use("/api/report", reportRoutes);
 
 export default app;
